6249c0c9f4de6387899f6f7e4c2278f4e02cd62e
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-01 15:10:20 -0300

    feat: create dataContext

---
+++ HealthMed/5 - Infra/Infra/Persistence/Sql/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Persistence.Sql.Context;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    { }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
