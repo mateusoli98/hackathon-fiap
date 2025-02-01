eeba94d398759a461523573183ca5193eefa906f
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-01 14:50:39 -0300

    feat: create entities

---
+++ HealthMed/4 - Domain/Domain/Entities/Patitent.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Patitent
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
