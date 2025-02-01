084014c952ddf82e9440ad45d8c3e0f8afc60a31
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-01 18:36:00 -0300

    feat: [use case] Create Doctor

---
+++ HealthMed/3 - Application/Application/UseCases/CreateDoctor/Commom/CreateDoctorRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.CreateDoctor.Commom;
+
+public class CreateDoctorRequestValidator : AbstractValidator<CreateDoctorRequest>
+{
+    public CreateDoctorRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("O nome é obrigatório.")
+            .Length(3, 100).WithMessage("O nome deve ter entre 3 e 100 caracteres.");
+
+        RuleFor(x => x.CRM)
+            .NotEmpty().WithMessage("O CRM é obrigatório.")
+            .Length(3, 10).WithMessage("O CRM deve ter entre 3 e 10 caracteres.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("O email é obrigatório.")
+            .EmailAddress().WithMessage("O email não está em um formato válido.");
+
+        RuleFor(x=>x.Password)
+            .NotEmpty().WithMessage("A senha é obrigatória.")
+            .MinimumLength(6).WithMessage("A senha deve ter entre 6 e 15 caracteres.")
+            .MaximumLength(15).WithMessage("A senha deve ter entre 6 e 15 caracteres.")
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,15}$")
+                .WithMessage("A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial");
+    }
+}
---
+++ HealthMed/3 - Application/Application/UseCases/CreateDoctor/Commom/CreateDoctorResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.UseCases.CreateDoctor.Commom;
+
+public class CreateDoctorResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string? Message { get; set; }
+}
