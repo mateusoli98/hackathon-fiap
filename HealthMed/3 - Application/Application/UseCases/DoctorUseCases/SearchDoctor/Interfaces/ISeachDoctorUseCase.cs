1ca96718f5b2c8ad4d7f05d4f3ef953817b31150
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-03 20:25:57 -0300

    feat: use case - search doctors

---
+++ HealthMed/3 - Application/Application/UseCases/DoctorUseCases/SearchDoctor/Interfaces/ISeachDoctorUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.UseCases.DoctorUseCases.SearchDoctor.Common;
+using Domain.DomainObjects.Filters;
+using ErrorOr;
+
+namespace Application.UseCases.DoctorUseCases.SearchDoctor.Interfaces;
+
+public interface ISeachDoctorUseCase
+{
+    Task<ErrorOr<PaginationResult<SearchDoctorResponse>>> Execute(DoctorFilter filter, CancellationToken cancellationToken = default);
+}
---
+++ HealthMed/3 - Application/Application/UseCases/DoctorUseCases/SearchDoctor/SearchDoctorUseCase.cs
@@ -0,0 +1,22 @@
+﻿using Application.UseCases.DoctorUseCases.SearchDoctor.Common;
+using Application.UseCases.DoctorUseCases.SearchDoctor.Interfaces;
+using Domain.DomainObjects.Filters;
+using Domain.Repositories.Relational;
+using ErrorOr;
+
+namespace Application.UseCases.DoctorUseCases.SearchDoctor;
+
+public  class SearchDoctorUseCase : ISeachDoctorUseCase
+{
+    readonly IDoctorRepository _doctorRepository;
+    public SearchDoctorUseCase(IDoctorRepository doctorRepository)
+    {
+        _doctorRepository = doctorRepository;
+    }
+    public async Task<ErrorOr<PaginationResult<SearchDoctorResponse>>> Execute(DoctorFilter filter, CancellationToken cancellationToken = default)
+    {
+        var result = await _doctorRepository.SearchAsync(filter, cancellationToken);
+
+        return new PaginationResult<SearchDoctorResponse>(result.Total, result.Items.Select(item => SearchDoctorResponse.Create(item)));
+    }
+}
