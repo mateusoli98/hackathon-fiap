384933b71b112c8a1c7b804e6b558c6e8314862b
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-03 19:34:57 -0300

    feat: use case - get doctor

---
+++ HealthMed/3 - Application/Application/UseCases/DoctorUseCases/GetDoctor/Interfaces/IGetDoctorUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Application.UseCases.DoctorUseCases.GetDoctor.Common;
+using ErrorOr;
+
+namespace Application.UseCases.DoctorUseCases.GetDoctor.Interfaces;
+
+public interface IGetDoctorUseCase
+{
+    Task<ErrorOr<GetDoctorResponse>> Execute(long id, CancellationToken cancellationToken = default);
+}
