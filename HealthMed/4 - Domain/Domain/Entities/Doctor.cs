e577bd67eb286d7e9a09638ba48ee111ecadbfa3
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-02-04 20:23:34 -0300

    feat: add SpecialtyEnum

--- HealthMed/4 - Domain/Domain/Entities/Doctor.cs
+++ HealthMed/4 - Domain/Domain/Entities/Doctor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Domain.DomainObjects.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities;
 
@@ -8,7 +9,7 @@ public class Doctor
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string CRM { get; set; } = string.Empty;
-    public string Specialty { get; set; } = string.Empty; // Um enumerador ou uma lista de algum lugar para limitar e padronizar esse campo?
+    public SpecialtyEnum Specialty { get; set; }
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty; //Isso deve ser salvo de forma hasheada
 }
